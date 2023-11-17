50344d5d1f62befa8be2de2fff48e8e04f73e975
aleksem07 <aleksem07@gmail.com>
2023-11-18 02:16:49 +0300

    feat: resolve

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -190,8 +190,16 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (typeof str !== 'string') {
+    return 0;
+  }
+
+  return str
+    .split('')
+    .map((char) => char.charCodeAt(0))
+    .reduce((a, b) => a + b, 0)
+    .toString();
 }
 
 /**
